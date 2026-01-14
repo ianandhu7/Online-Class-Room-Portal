5a8838b0a8176322f3287d222063bb6cf0b72e8b
anandhuviju <anandhuviju7@gmail.com>
2026-01-14 23:29:39 +0530

    Initial commit

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,14 +3,14 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegisterView, UserProfileView, CustomTokenObtainPairView, UserListView, GlobalStatsView, TeacherStatsView, StudentStatsView, UserMessagingListView, UserDetailView
 
 urlpatterns = [
-    path('register', RegisterView.as_view(), name='register'),
-    path('login', CustomTokenObtainPairView.as_view(), name='login'),
-    path('me', UserProfileView.as_view(), name='profile'),
-    path('refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users', UserListView.as_view(), name='user-list'),
-    path('users/<int:pk>', UserDetailView.as_view(), name='user-detail'),
-    path('search-users', UserMessagingListView.as_view(), name='search-users'),
-    path('stats', GlobalStatsView.as_view(), name='global-stats'),
-    path('teacher-stats', TeacherStatsView.as_view(), name='teacher-stats'),
-    path('student-stats', StudentStatsView.as_view(), name='student-stats'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('me/', UserProfileView.as_view(), name='profile'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('search-users/', UserMessagingListView.as_view(), name='search-users'),
+    path('stats/', GlobalStatsView.as_view(), name='global-stats'),
+    path('teacher-stats/', TeacherStatsView.as_view(), name='teacher-stats'),
+    path('student-stats/', StudentStatsView.as_view(), name='student-stats'),
 ]
